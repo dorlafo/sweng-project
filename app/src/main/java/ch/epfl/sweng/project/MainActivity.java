7388b215357b9a6df25d8c27265b1268ad7195ca
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2016-10-07 10:41:14 +0200

    Fix android studio git conflicts

--- app/src/main/java/ch/epfl/sweng/project/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/project/MainActivity.java
@@ -7,7 +7,12 @@ import android.os.Bundle;
  * Your app's main activity.
  */
 public final class MainActivity extends AppCompatActivity {
-    
+    // TODO replace this code with your app code!
+
+    public static int add(final int a, final int b) {
+        return a + b;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
