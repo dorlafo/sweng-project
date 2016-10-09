c7823c136e27495af88f9ea341ad9f06b69f7867
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2016-10-09 12:58:35 +0200

    Add methods to satisfy Firebase compatibility and implement equals method

--- app/src/main/java/ch/epfl/sweng/project/model/ID.java
+++ app/src/main/java/ch/epfl/sweng/project/model/ID.java
@@ -4,14 +4,23 @@ package ch.epfl.sweng.project.model;
  * @author Amaury Combes
  */
 
-public abstract class ID {
-    private final long id;
+abstract class ID {
+    private long id;
 
-    protected ID(long id) {
+    public ID() {
+        // Default constructor required for calls to DataSnapshot.getValue
+    }
+
+    ID(long id) {
         this.id = id;
     }
 
     public long getID() {
         return id;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other instanceof ID && ((ID) other).getID() == this.getID();
     }
 }
