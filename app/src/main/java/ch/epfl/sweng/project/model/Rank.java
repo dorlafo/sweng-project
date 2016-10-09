f38a747bf21e2fccf5b71d34324436bd5b626b20
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2016-10-09 13:02:38 +0200

    Add default constructor to comply with Firebase requisites and implement Comparable (in case wey want to easily sort by rank)

--- app/src/main/java/ch/epfl/sweng/project/model/Rank.java
+++ app/src/main/java/ch/epfl/sweng/project/model/Rank.java
@@ -4,14 +4,20 @@ package ch.epfl.sweng.project.model;
  * @author Amaury Combes
  */
 
-public abstract class Rank {
-    private final int rank;
+public abstract class Rank implements Comparable<Rank> {
+    private int rank;
 
-    protected Rank(int rank) {
+    public Rank() { }
+    public Rank(int rank) {
         this.rank = rank;
     }
 
     public int getRank() {
         return rank;
+    }
+
+    @Override
+    public int compareTo(Rank o) {
+        return  ((Integer) rank).compareTo(o.getRank());
     }
 }
