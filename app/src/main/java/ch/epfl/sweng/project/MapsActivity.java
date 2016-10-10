dd492ce5d2a80b3fb0307715962ef85638d75d42
panv <panv@hotmail.fr>
2016-10-10 22:48:15 +0200

    Fix OutOfMemory Error and improve display of InfoWindow

--- app/src/main/java/ch/epfl/sweng/project/MapsActivity.java
+++ app/src/main/java/ch/epfl/sweng/project/MapsActivity.java
@@ -1,31 +1,44 @@
 package ch.epfl.sweng.project;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
-import android.Manifest;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.InfoWindowAdapter;
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 
 import ch.epfl.sweng.project.model.Match;
-
 import ch.epfl.sweng.project.model.Player;
-import ch.epfl.sweng.project.res.DummyMatchData; // Used for test purposes
+import ch.epfl.sweng.project.res.DummyMatchData; // TODO: delete when real data is used
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap matchMap;
-    private LatLng currentPositionCoord;
+    // private LatLng currentPositionCoord;
 
     private final int MY_LOCATION_REQUEST_CODE = 39;
 
@@ -44,6 +57,41 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         matchMap = googleMap;
         matchMap.getUiSettings().setZoomControlsEnabled(true);
+        matchMap.setInfoWindowAdapter(new InfoWindowAdapter() {
+            @Override
+            public View getInfoWindow(Marker marker) {
+                return null;
+            }
+
+            @Override
+            public View getInfoContents(Marker marker) {
+                Context context = MapsActivity.this;
+
+                LinearLayout infoWindow = new LinearLayout(context);
+                infoWindow.setOrientation(LinearLayout.VERTICAL);
+
+                TextView title = new TextView(context);
+                title.setTextColor(Color.BLACK);
+                title.setGravity(Gravity.CENTER);
+                title.setTypeface(null, Typeface.BOLD);
+                title.setText(marker.getTitle());
+
+                TextView snippet = new TextView(context);
+                snippet.setTextColor(Color.GRAY);
+                snippet.setText(marker.getSnippet());
+
+                infoWindow.addView(title);
+                infoWindow.addView(snippet);
+
+                return infoWindow;
+            }
+        });
+        matchMap.setOnInfoWindowClickListener(new OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                // TODO: make clicking on the info window join the match
+            }
+        });
 
         // TODO: change to use real matches
         displayNearbyMatches(DummyMatchData.dummyMatches());
@@ -54,18 +102,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             matchMap.setMyLocationEnabled(true);
         } else {
             ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_LOCATION_REQUEST_CODE);
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_LOCATION_REQUEST_CODE);
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
+                                           String permissions[],
+                                           int[] grantResults) {
         switch (requestCode) {
             case MY_LOCATION_REQUEST_CODE:
-                if (permissions.length == 1 &&
-                        permissions[0].equals(Manifest.permission.ACCESS_FINE_LOCATION) &&
-                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (permissions.length == 1
+                        && permissions[0].equals(Manifest.permission.ACCESS_FINE_LOCATION)
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     //matchMap.setMyLocationEnabled(true);
                 }
         }
@@ -85,18 +135,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     private String markerSnippet(Match match) {
-        StringBuilder builder = new StringBuilder(R.string.snippet_match_rank);
-        builder.append(match.getRank().getRank())
-                .append(System.getProperty("line.separator"))
-                .append(R.string.snippet_player_list)
+        Resources res = getResources();
+        String newLine = System.getProperty("line.separator");
+
+        StringBuilder builder = new StringBuilder(
+                res.getString(R.string.snippet_match_rank))
+                .append(match.getRank().getRank())
+                .append(newLine)
+                .append(res.getString(R.string.snippet_player_list))
                 .append(listToString(match.getPlayers()))
-                .append(System.getProperty("line.separator"))
-                .append(R.string.snippet_game_variant)
-                //.append(match.getGameVariant().toString()) TODO: implement this
-                .append(System.getProperty("line.separator"))
-                .append(R.string.snippet_expiration_date)
-                .append(match.getExpirationTime().toString());
+                .append(newLine)
+                .append(res.getString(R.string.snippet_game_variant))
+                //builder.append(match.getGameVariant().toString()) TODO: implement this
+                .append(newLine)
+                .append(res.getString(R.string.snippet_expiration_date))
+                .append(dateToStringCustom(match.getExpirationTime()));
+
         return builder.toString();
+    }
+
+    private String dateToStringCustom(Date date) {
+        DateFormat dateFormat = new SimpleDateFormat(
+                getResources().getString(R.string.date_format),
+                Locale.FRENCH);
+        return dateFormat.format(date);
     }
 
     private String listToString(List<Player> players) {
