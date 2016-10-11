32200fddf619569edde58f22e31e2eeba5631a11
panv <panv@hotmail.fr>
2016-10-11 23:15:46 +0200

    Add custom adapter

--- app/src/main/java/ch/epfl/sweng/project/tools/CustomAdapter.java
+++ app/src/main/java/ch/epfl/sweng/project/tools/CustomAdapter.java
@@ -1,28 +1,65 @@
 package ch.epfl.sweng.project.tools;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
+import ch.epfl.sweng.project.R;
 import ch.epfl.sweng.project.model.Match;
 
-import com.google.common.collect.Iterables;
-
-
-/**
- * Created by phanvan on 11/10/16.
- */
+import java.util.List;
 
 public class CustomAdapter extends BaseAdapter {
     private Context context;
-    private Iterable<Match> matches;
+    private List<Match> matches;
 
-    public CustomAdapter(Context context, Iterable<Match> matches) {
+    public CustomAdapter(Context context, List<Match> matches) {
         this.context = context;
         this.matches = matches;
     }
 
     @Override
     public int getCount() {
-        return Iterables.size(matches);
+        return matches.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return matches.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.
+                    getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = (View) inflater.inflate(R.layout.list_row, null);
+        }
+
+        TextView description = (TextView) convertView.findViewById(R.id.description);
+        TextView rankData = (TextView) convertView.findViewById(R.id.rank_data);
+        TextView players = (TextView) convertView.findViewById(R.id.players_data);
+        TextView variant = (TextView) convertView.findViewById(R.id.variant_data);
+        TextView expirationDate = (TextView) convertView.findViewById(R.id.expiration_date_data);
+
+        Match match = matches.get(position);
+        MatchStringifier stringifier = new MatchStringifier(context);
+        stringifier.setMatch(match);
+
+        description.setText(match.getDescription());
+        rankData.setText(stringifier.rankToString());
+        players.setText(stringifier.playersToString());
+        // variant.setText(stringifier.variantToString()); TODO: implement this
+        expirationDate.setText(stringifier.dateToStringCustom());
+
+        return convertView;
     }
 }
