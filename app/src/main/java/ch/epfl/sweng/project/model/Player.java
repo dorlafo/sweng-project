53b0913f2df724894e674c70f6a49b450d757e77
Amaury Combes <amaurycombes@hotmail.fr>
2016-10-14 12:24:09 +0200

    Add documentation to the Player class

--- app/src/main/java/ch/epfl/sweng/project/model/Player.java
+++ app/src/main/java/ch/epfl/sweng/project/model/Player.java
@@ -3,7 +3,10 @@ package ch.epfl.sweng.project.model;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * Player is class that represents a player
+ * It implements the Averageable interface
+ */
 public class Player implements Averageable<Player, Rank> {
     private PlayerID id;
     private String lastName;
@@ -17,6 +20,13 @@ public class Player implements Averageable<Player, Rank> {
 
     }
 
+    /**
+     * Constructor of the Player class with no default assignement
+     * @param id the id of the player (sciper)
+     * @param lastName the last name of the player
+     * @param firstName the first name of the player
+     * @param rank the rank of the player
+     */
     public Player(PlayerID id, String lastName, String firstName, Rank rank) {
         this.id = id;
         this.lastName = lastName;
@@ -24,22 +34,42 @@ public class Player implements Averageable<Player, Rank> {
         this.rank = rank;
     }
 
+    /**
+     * Getter for the id of the player
+     * @return returns the id of the player
+     */
     public PlayerID getID() {
         return id;
     }
 
+    /**
+     * Getter for the last name of the player
+     * @return returns the last name of the player
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Getter for the first name of the player
+     * @return returns the first name of the player
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Getter for the rank of the player
+     * @return returns the rank of the player
+     */
     public Rank getRank() {
         return rank;
     }
 
+    /**
+     * Sets the rank of the player
+     * @param newRank the new value of the rank we have to set
+     */
     public void setRank(Rank newRank) {
         this.rank = newRank;
     }
