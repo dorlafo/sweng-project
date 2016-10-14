beedc39558c878612157a09197ff4d93819d34f4
panv <panv@hotmail.fr>
2016-10-14 22:23:55 +0200

    Fix tests

--- app/src/androidTest/java/ch/epfl/sweng/project/MapsActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/project/MapsActivityTest.java
@@ -2,6 +2,7 @@ package ch.epfl.sweng.project;
 
 import android.support.test.rule.ActivityTestRule;
 
+import org.junit.Rule;
 import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
@@ -10,14 +11,14 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
-public final class MapsActivityTest extends ActivityTestRule<MapsActivity> {
-    public MapsActivityTest() {
-        super(MapsActivity.class);
-    }
+public final class MapsActivityTest {
+
+    @Rule
+    public ActivityTestRule<MapsActivity> activityTestRule =
+            new ActivityTestRule<>(MapsActivity.class);
 
     @Test
     public void testSwitchToListFromMap() {
-        getActivity();
         onView(withId(R.id.switch_to_list)).perform(click());
         onView(withId(android.R.id.list)).check(matches(isEnabled()));
     }
--- app/src/androidTest/java/ch/epfl/sweng/project/MatchListActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/project/MatchListActivityTest.java
@@ -2,6 +2,7 @@ package ch.epfl.sweng.project;
 
 import android.support.test.rule.ActivityTestRule;
 
+import org.junit.Rule;
 import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
@@ -10,14 +11,14 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
-public final class MatchListActivityTest extends ActivityTestRule<MatchListActivity> {
-    public MatchListActivityTest() {
-        super(MatchListActivity.class);
-    }
+public final class MatchListActivityTest {
+
+    @Rule
+    public ActivityTestRule<MatchListActivity> activityTestRule =
+            new ActivityTestRule<>(MatchListActivity.class);
 
     @Test
     public void testSwitchToMapFromList() {
-        getActivity();
         onView(withId(R.id.switch_to_map)).perform(click());
         onView(withId(R.id.switch_to_list)).check(matches(isDisplayed()));
     }
