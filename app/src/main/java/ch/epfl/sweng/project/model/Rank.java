aae096bac07fb99858d3cae8c529c1b941967090
Amaury Combes <amaurycombes@hotmail.fr>
2016-10-14 12:15:38 +0200

    Add documentation to the Rank class

--- app/src/main/java/ch/epfl/sweng/project/model/Rank.java
+++ app/src/main/java/ch/epfl/sweng/project/model/Rank.java
@@ -2,18 +2,32 @@ package ch.epfl.sweng.project.model;
 
 import java.util.List;
 
-
+/**
+ * Rank is class that represents a rank in a competition system.
+ * It implements the Averageable interface and the Comparable interface
+ */
 public class Rank implements Averageable<Rank, Rank>, Comparable<Rank> {
     private int rank;
 
+    /**
+     * Default constructor required for calls to DataSnapshot.getValue when using Firebase
+     */
     public Rank() {
 
     }
 
+    /**
+     * Constructor of the Rank class
+     * @param rank the value of the rank
+     */
     public Rank(int rank) {
         this.rank = rank;
     }
 
+    /**
+     * Provides a string representation for the Rank class
+     * @return returns a string representation of the Rank class
+     */
     public String toSring() {
         return Integer.toString(rank);
     }
