ca294830371fdb954b053647ee269bc3b93c7290
panv <panv@hotmail.fr>
2016-10-16 17:25:41 +0200

    Fix tests 3 - Maybe for real this time

--- app/src/androidTest/java/ch/epfl/sweng/project/MapsActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/project/MapsActivityTest.java
@@ -1,9 +1,13 @@
 package ch.epfl.sweng.project;
 
-import android.support.test.rule.ActivityTestRule;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.ActivityInstrumentationTestCase2;
 
-import org.junit.Rule;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -11,15 +15,31 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
-public final class MapsActivityTest {
+@RunWith(AndroidJUnit4.class)
+public final class MapsActivityTest extends
+        ActivityInstrumentationTestCase2<MapsActivity> {
 
-    @Rule
-    public ActivityTestRule<MapsActivity> activityTestRule =
-            new ActivityTestRule<>(MapsActivity.class);
+    private MapsActivity activity;
+
+    public MapsActivityTest() {
+        super(MapsActivity.class);
+    }
+
+    @Before
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+        activity = getActivity();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
 
     @Test
     public void testSwitchToListFromMap() {
-        activityTestRule.getActivity();
         onView(withId(R.id.switch_to_list)).perform(click());
         onView(withId(android.R.id.list)).check(matches(isEnabled()));
     }
--- app/src/androidTest/java/ch/epfl/sweng/project/MatchListActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/project/MatchListActivityTest.java
@@ -1,9 +1,13 @@
 package ch.epfl.sweng.project;
 
-import android.support.test.rule.ActivityTestRule;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.ActivityInstrumentationTestCase2;
 
-import org.junit.Rule;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -11,15 +15,31 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
-public final class MatchListActivityTest {
+@RunWith(AndroidJUnit4.class)
+public final class MatchListActivityTest extends
+        ActivityInstrumentationTestCase2<MatchListActivity> {
 
-    @Rule
-    public ActivityTestRule<MatchListActivity> activityTestRule =
-            new ActivityTestRule<>(MatchListActivity.class);
+    private MatchListActivity activity;
+
+    public MatchListActivityTest() {
+        super(MatchListActivity.class);
+    }
+
+    @Before
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+        activity = getActivity();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
 
     @Test
     public void testSwitchToMapFromList() {
-        activityTestRule.getActivity();
         onView(withId(R.id.switch_to_map)).perform(click());
         onView(withId(R.id.switch_to_list)).check(matches(isDisplayed()));
     }
