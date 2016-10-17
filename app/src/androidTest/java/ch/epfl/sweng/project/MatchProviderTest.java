67a3cf72256ad4a04c44f9d7231c51c7b326cb53
Amaury Combes <amaurycombes@hotmail.fr>
2016-10-17 19:53:34 +0200

    Add the test hierarchy and the MatchProviderTest class

---
+++ app/src/androidTest/java/ch/epfl/sweng/project/MatchProviderTest.java
@@ -0,0 +1,23 @@
+package ch.epfl.sweng.project;
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.ActivityInstrumentationTestCase2;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+/**
+ * Created by Amaury on 17/10/2016.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class MatchProviderTest {
+
+
+    @Test
+    public void test() {
+
+    }
+}
