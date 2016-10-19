9a50f964117a4344faa41c93142bda1fe79052b8
dorlafo <dorian.laforest@epfl.ch>
2016-10-19 08:38:30 +0200

    Add empty method in UserProvider class

--- app/src/main/java/ch/epfl/sweng/project/database/UserProvider.java
+++ app/src/main/java/ch/epfl/sweng/project/database/UserProvider.java
@@ -1,8 +1,29 @@
 package ch.epfl.sweng.project.database;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ch.epfl.sweng.project.model.Player;
+
 public class UserProvider {
 
+    //UserProvider constructor
+    //Make the connection to the user table
     public UserProvider() {
 
     }
+
+    //Close the connection to the user table
+    public void close() {
+
+    }
+
+    //Get the user list
+    public List<Player> providePlayers() {
+        List<Player> players = new ArrayList<>();
+
+        return players;
+    }
+
+    //Get one user
 }
