25f9449f4c68def23a35e8fa3f2d74cf7f02c31b
panv <panv@hotmail.fr>
2016-10-19 15:30:51 +0200

    Add event listener for privacy switch and description text field

--- app/src/main/java/ch/epfl/sweng/project/CreateMatchActivity.java
+++ app/src/main/java/ch/epfl/sweng/project/CreateMatchActivity.java
@@ -6,7 +6,10 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Switch;
 
 import ch.epfl.sweng.project.database.MatchProvider;
 import ch.epfl.sweng.project.model.Match;
@@ -28,6 +31,17 @@ public class CreateMatchActivity extends AppCompatActivity
         Button createMatch = (Button) findViewById(R.id.create_create_button);
         createMatch.setOnClickListener(this);
 
+        Button confirmDescription = (Button) findViewById(R.id.description_confirmation);
+        confirmDescription.setOnClickListener(this);
+
+        Switch privacySwitch = (Switch) findViewById(R.id.switch_private);
+        privacySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                matchBuilder.setPrivacy(isChecked);
+            }
+        });
+
         Spinner playerNumSpinner = (Spinner) findViewById(R.id.player_num_spinner);
         Spinner variantSpinner = (Spinner) findViewById(R.id.variant_spinner);
         ArrayAdapter<CharSequence> playerNumAdapter = ArrayAdapter.createFromResource(this,
@@ -48,7 +62,11 @@ public class CreateMatchActivity extends AppCompatActivity
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.create_create_button:
-                //publishMatch();
+                publishMatch(matchBuilder.build());
+                break;
+            case R.id.description_confirmation:
+                EditText editDescription = (EditText) findViewById(R.id.description_match_text);
+                matchBuilder.setDescription(editDescription.getText().toString());
                 break;
             default:
                 break;
