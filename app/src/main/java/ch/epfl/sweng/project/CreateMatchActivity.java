9bf0c22d7031e7a147ddf77755681c1cd6aa60d4
panv <panv@hotmail.fr>
2016-10-19 14:45:23 +0200

    Add create match button

--- app/src/main/java/ch/epfl/sweng/project/CreateMatchActivity.java
+++ app/src/main/java/ch/epfl/sweng/project/CreateMatchActivity.java
@@ -1,24 +1,28 @@
 package ch.epfl.sweng.project;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
-import ch.epfl.sweng.project.model.Match;
 import ch.epfl.sweng.project.database.MatchProvider;
+import ch.epfl.sweng.project.model.Match;
 
 public class CreateMatchActivity extends AppCompatActivity
-        implements View.OnClickListener {
-    MatchProvider mProvider;
+        implements OnClickListener {
+    private MatchProvider mProvider;
+    private Match.Builder matchBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_match);
+
         mProvider = new MatchProvider();
+        matchBuilder = new Match.Builder();
 
         // getId of all buttons
         Button createMatch = (Button) findViewById(R.id.create_create_button);
