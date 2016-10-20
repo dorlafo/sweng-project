3489904aa355c6ea93b5a6557da0cb874ce8aba8
Amaury Combes <amaurycombes@hotmail.fr>
2016-10-20 15:10:01 +0200

    Add some comments to the MatchDatabaseInterface

--- app/src/main/java/ch/epfl/sweng/project/database/MatchDatabaseInterface.java
+++ app/src/main/java/ch/epfl/sweng/project/database/MatchDatabaseInterface.java
@@ -15,7 +15,7 @@ import java.util.Map;
 import ch.epfl.sweng.project.model.Match;
 
 /**
- * MatchDatabaseInterface is a class that provides operation to interact with the Firebase server.
+ * MatchDatabaseInterface is a class that provides operations to interact with the Firebase server.
  */
 public class MatchDatabaseInterface {
     private final Map<String, Match> matches;
@@ -23,6 +23,9 @@ public class MatchDatabaseInterface {
     private final ChildEventListener matchListener;
     private final DatabaseReference.CompletionListener completionListener;
 
+    /**
+     * Constructor of the MatchDatabaseInterface.
+     */
     public MatchDatabaseInterface() {
         matches = new HashMap<>();
         dRef = FirebaseDatabase.getInstance().getReference("matches");
@@ -31,28 +34,55 @@ public class MatchDatabaseInterface {
         completionListener = new MatchCompletionListener();
     }
 
+    /**
+     * Closes the listenner between the app and the Firebase server. Thus the updates in the database won't be seen in
+     * the app
+     */
     public void close() {
         dRef.removeEventListener(matchListener);
     }
 
+    /**
+     * Provides the set of match stored in the database
+     * @return returns the matches stored in the database as Map with the id of the match as key and the Match object as
+     * value
+     */
     public Map<String, Match> provide() {
         return matches;
     }
 
+    /**
+     * This method let us write a new match to the Firebase database
+     * @param matchToWrite the new match to write
+     * @return returns the token of the match that was just written in the database
+     */
     public String writeNewMatch(Match matchToWrite) {
         dRef.push().setValue(matchToWrite, completionListener);
         String token = dRef.getKey();
         return token;
     }
 
+    /**
+     * This method let us delete a match from the Firebase database
+     * @param token the id of the match that should be deleted
+     */
     public void deleteMatch(String token) {
         dRef.getRoot().child(token).removeValue(completionListener);
     }
 
+    /**
+     * This method let us update (write) a match to the database
+     * @param id the id of the match that should be overwrite
+     * @param match the data of the match that should be overwrite
+     */
     public void updateMatch(String id, Match match) {
         dRef.child(id).setValue(match, completionListener);
     }
 
+    /**
+     * MatchCompletionListener is a class that let us know if an operation was not able to get to the server
+     * It implements the CompletionListener
+     */
     public class MatchCompletionListener implements CompletionListener {
 
 
@@ -66,6 +96,11 @@ public class MatchDatabaseInterface {
 
     }
 
+    /**
+     * MatchEventListener is class that is needed to update our internal representation of the database. It listens to
+     * any changes in the Firebase database and updates our data.
+     * It implements the ChildEventListener
+     */
     private class MatchEventListener implements ChildEventListener {
         @Override
         public void onChildAdded(DataSnapshot dataSnapshot, String s) {
--- app/src/test/java/ch/epfl/sweng/project/RankTest.java
+++ app/src/test/java/ch/epfl/sweng/project/RankTest.java
@@ -1,8 +1,6 @@
 package ch.epfl.sweng.project;
 
-/**
- * Created by Amaury on 20/10/2016.
- */
 
 public final class RankTest {
+
 }
