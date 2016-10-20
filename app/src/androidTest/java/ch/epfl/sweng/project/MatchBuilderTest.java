099306b35f2dcf1caf7bd271fd133f77cd65b502
panv <panv@hotmail.fr>
2016-10-20 23:54:01 +0200

    Fix test

--- app/src/androidTest/java/ch/epfl/sweng/project/MatchBuilderTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/project/MatchBuilderTest.java
@@ -28,14 +28,14 @@ public final class MatchBuilderTest {
 
         Assert.assertFalse(match.getPlayers().isEmpty());
         Assert.assertEquals(46.520407, match.getLocation().getLatitude());
-        Assert.assertEquals(6.565802, match.getLocation().getLatitude());
+        Assert.assertEquals(6.565802, match.getLocation().getLongitude());
         Assert.assertEquals("New Match", match.getDescription());
-        Assert.assertEquals(0, match.getRank());
+        Assert.assertEquals(123, match.getRank().getRank());
         Assert.assertFalse(match.isPrivateMatch());
         Assert.assertEquals(CLASSIC, match.getGameVariant());
         Assert.assertEquals(4, match.getMaxPlayerNumber());
         Assert.assertEquals(Calendar.getInstance().getTimeInMillis() + 2 * 3600 * 1000,
-                match.getExpirationTime(), 60);
+                match.getExpirationTime(), 10);
     }
 
     @Test
@@ -44,7 +44,10 @@ public final class MatchBuilderTest {
         matchBuilder.addPlayer(new Player(new PlayerID(1), "Combes", "Amaury", new Rank(123)));
         Match match = matchBuilder.build();
 
-        Assert.assertEquals("Amaury Combes", match.getPlayers().get(0).toString());
+        Player player = match.getPlayers().get(0);
+
+        Assert.assertEquals("Amaury", player.getFirstName());
+        Assert.assertEquals("Combes", player.getLastName());
     }
 
 }
