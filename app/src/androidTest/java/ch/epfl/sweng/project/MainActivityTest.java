875201c220ad632dc608970ed9eb5290d9a74772
panv <panv@hotmail.fr>
2016-10-21 15:05:46 +0200

    Add MainActivity test

--- app/src/androidTest/java/ch/epfl/sweng/project/MainActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/project/MainActivityTest.java
@@ -1,5 +1,33 @@
 package ch.epfl.sweng.project;
 
-public final class MainActivityTest {
+import android.support.test.InstrumentationRegistry;
+import android.test.ActivityInstrumentationTestCase2;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+public final class MainActivityTest extends
+        ActivityInstrumentationTestCase2<MainActivity> {
+
+    private MainActivity activity;
+
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+        activity = getActivity();
+    }
+
+    public void createMatchButtonSwitchesToCorrectActivity() {
+        onView(withId(R.id.create_match_button)).perform(click());
+        onView(withId(R.id.create_title)).check(matches(isDisplayed()));
+    }
 
 }
--- app/src/main/java/ch/epfl/sweng/project/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/project/MainActivity.java
@@ -1,8 +1,8 @@
 package ch.epfl.sweng.project;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import ch.epfl.sweng.project.database.MatchProvider;
