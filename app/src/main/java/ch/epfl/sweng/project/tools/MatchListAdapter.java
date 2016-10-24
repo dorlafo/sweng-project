f9721bb5653c423d1e8154d1da9c1ffed18d9193
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2016-10-24 13:43:24 +0200

    MapsActivity now pulls matches from db and shows them in real time (modification, deletion)

--- app/src/main/java/ch/epfl/sweng/project/tools/MatchListAdapter.java
+++ app/src/main/java/ch/epfl/sweng/project/tools/MatchListAdapter.java
@@ -21,18 +21,18 @@ public class MatchListAdapter extends FirebaseListAdapter<Match> {
 
     @Override
     protected void populateView(View v, Match match, int position) {
-            TextView description = (TextView) v.findViewById(R.id.description);
-            TextView rankData = (TextView) v.findViewById(R.id.rank_data);
-            TextView players = (TextView) v.findViewById(R.id.players_data);
-            TextView variant = (TextView) v.findViewById(R.id.variant_data);
-            TextView expirationDate = (TextView) v.findViewById(R.id.expiration_date_data);
+        TextView description = (TextView) v.findViewById(R.id.description);
+        TextView rankData = (TextView) v.findViewById(R.id.rank_data);
+        TextView players = (TextView) v.findViewById(R.id.players_data);
+        TextView variant = (TextView) v.findViewById(R.id.variant_data);
+        TextView expirationDate = (TextView) v.findViewById(R.id.expiration_date_data);
 
-            MatchStringifier stringifier = new MatchStringifier(activity);
-            stringifier.setMatch(match);
-            description.setText(match.getDescription());
-            rankData.setText(stringifier.rankToString());
-            players.setText(stringifier.playersToString());
-            variant.setText(stringifier.variantToString());
-            expirationDate.setText(stringifier.dateToStringCustom());
+        MatchStringifier stringifier = new MatchStringifier(activity);
+        stringifier.setMatch(match);
+        description.setText(match.getDescription());
+        rankData.setText(stringifier.rankToString());
+        players.setText(stringifier.playersToString());
+        variant.setText(stringifier.variantToString());
+        expirationDate.setText(stringifier.dateToStringCustom());
     }
 }
