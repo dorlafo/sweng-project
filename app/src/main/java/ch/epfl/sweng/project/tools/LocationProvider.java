1e6846c5237aa970b3c75862e021ce0cca7501ed
panv <panv@hotmail.fr>
2016-10-26 01:44:40 +0200

    Rename LocationHandler to LocationProvider and finish implementation

---
+++ app/src/main/java/ch/epfl/sweng/project/tools/LocationProvider.java
@@ -0,0 +1,99 @@
+package ch.epfl.sweng.project.tools;
+
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public final class LocationProvider implements ConnectionCallbacks, LocationListener {
+
+    private LocationProviderListener providerListener;
+
+    private final Context context;
+    private GoogleApiClient googleApiClient;
+    private LocationRequest locationRequest;
+
+    public LocationProvider(Context context) {
+        this.context = context;
+        buildGoogleApiClient();
+        createLocationRequest();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ContextCompat.checkSelfPermission(context,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            Location location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+            if (location != null) {
+                notifyListener(location);
+            }
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        notifyListener(location);
+    }
+
+    public void setProviderListener(LocationProviderListener providerListener) {
+        this.providerListener = providerListener;
+    }
+
+    public void connectGoogleApiClient() {
+        googleApiClient.connect();
+    }
+
+    public void stopLocationUpdates() {
+        if (googleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
+            googleApiClient.disconnect();
+        }
+    }
+
+    private synchronized void buildGoogleApiClient() {
+        googleApiClient = new GoogleApiClient.Builder(context)
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    private void createLocationRequest() {
+        locationRequest = LocationRequest.create()
+                .setInterval(5000)
+                .setFastestInterval(1000)
+                .setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+    }
+
+    private void startLocationUpdates() {
+        if (ContextCompat.checkSelfPermission(context,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi
+                    .requestLocationUpdates(googleApiClient, locationRequest, this);
+        }
+    }
+
+    private void notifyListener(Location location) {
+        if (providerListener != null) {
+            providerListener.onLocationChanged(location);
+        }
+    }
+
+}
