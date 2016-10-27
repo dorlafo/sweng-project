1fe75d38b52816b24183b069fc5247499edb0c9a
Alexis Montavon <alexis.montavon@epfl.ch>
2016-10-27 14:52:53 +0200

    Add mathID to Match. Add join Match function. Modified Create Match in consequence

--- app/src/main/java/ch/epfl/sweng/project/MatchListActivity.java
+++ app/src/main/java/ch/epfl/sweng/project/MatchListActivity.java
@@ -1,12 +1,25 @@
 package ch.epfl.sweng.project;
 
 import android.app.ListActivity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.ListView;
 
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import ch.epfl.sweng.project.model.Match;
+import ch.epfl.sweng.project.model.Player;
+import ch.epfl.sweng.project.tequila.AuthClient;
 import ch.epfl.sweng.project.tools.MatchListAdapter;
 
 /**
@@ -16,6 +29,8 @@ import ch.epfl.sweng.project.tools.MatchListAdapter;
  */
 public class MatchListActivity extends ListActivity {
     MatchListAdapter mAdapter;
+    String sciper;
+    Player player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +40,53 @@ public class MatchListActivity extends ListActivity {
         ListView listView = (ListView) findViewById(android.R.id.list);
         listView.setEmptyView(findViewById(android.R.id.empty));
 
-        // TODO: fix empty list and filter private matches
+        // TODO: fix empty list
         mAdapter = new MatchListAdapter(this);
 
         listView.setAdapter(mAdapter);
     }
 
     @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
+    protected void onListItemClick(ListView l, View v, final int position, long id) {
+        // Opens dialog box to alert user of the authentication fail
+        // Allows user to cancel or retry
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.join_match)
+                .setMessage(R.string.join_message)
+                .setPositiveButton(R.string.join, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+                        Match match = mAdapter.getItem(position);
+                        String matchID = match.getMatchID();
+                        sciper = FirebaseAuth.getInstance().getCurrentUser().getDisplayName();
+                        FirebaseDatabase.getInstance().getReference()
+                                .child("players")
+                                .child(sciper)
+                                .addListenerForSingleValueEvent(new ValueEventListener(){
+
+                                    @Override
+                                    public void onDataChange(DataSnapshot dataSnapshot) {
+                                        player = dataSnapshot.getValue(Player.class);
+                                    }
+
+                                    @Override
+                                    public void onCancelled(DatabaseError databaseError) {
+
+                                    }
+                                });
+                        match.addPlayer(player);
+                        ref.child(matchID).setValue(match);
+                        Intent moveToMatchActivity = new Intent(MatchListActivity.this, MatchActivity.class);
+                        getIntent().putExtra("MATCH_ID", matchID);
+                        startActivity(moveToMatchActivity);
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Do nothing, goes back to ListMatchActivity
+                    }
+                })
+                .show();
         super.onListItemClick(l, v, position, id);
     }
 
