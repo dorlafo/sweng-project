6f348a2c58fd489d3a30a16023b2a399fb3ef196
Amaury Combes <amaurycombes@hotmail.fr>
2016-10-30 19:02:09 +0100

    Add the App class

---
+++ app/src/main/java/ch/epfl/sweng/project/App.java
@@ -0,0 +1,26 @@
+package ch.epfl.sweng.project;
+
+import android.app.Application;
+
+import ch.epfl.sweng.project.dagger.components.Graph;
+
+public class App extends Application {
+    private static App sInstance;
+    private Graph graph;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sInstance = this;
+        graph = Graph.Initializer.init(false);
+    }
+    public static App getInstance() {
+        return sInstance;
+    }
+    public Graph graph() {
+        return graph;
+    }
+    public void setMockMode(boolean useMock) {
+        graph = Graph.Initializer.init(useMock);
+    }
+}
