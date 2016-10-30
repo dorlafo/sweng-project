dbf5e0362189a8010d6a0e16f76c4a9935d2f3bc
Amaury Combes <amaurycombes@hotmail.fr>
2016-10-30 17:18:03 +0100

    Add the DebugDataModule class which contains a set of providers.

---
+++ app/src/main/java/ch/epfl/sweng/project/modules/DebugDataModule.java
@@ -0,0 +1,41 @@
+package ch.epfl.sweng.project.modules;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+import static org.mockito.Mockito.mock;
+
+/**
+ * DebugDataModule is module (i.e. a class that defines a set of providers which are the method
+ * annotated with @Provides)
+ */
+@Module
+public final class DebugDataModule {
+    private final boolean mockMode;
+
+    /**
+     * Constructor of the DebugDataModule class
+     * @param provideMocks let us enable the mocking of our objects returned by the providers
+     */
+    public DebugDataModule(boolean provideMocks) {
+        this.mockMode = provideMocks;
+    }
+
+    /**
+     * A DatabaseReference provider
+     * @return returns a DatabaseReference that can be mocked or not
+     */
+    @Provides @Singleton
+    public DatabaseReference provideDBReference() {
+        if(mockMode) {
+            return mock(DatabaseReference.class);
+        } else {
+            return FirebaseDatabase.getInstance().getReference();
+        }
+    }
+}
