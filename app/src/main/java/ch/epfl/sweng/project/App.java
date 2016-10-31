42c824d9fd63cd9dfff3c46c4b5cb5b5253e2e6b
Amaury Combes <amaurycombes@hotmail.fr>
2016-10-31 11:36:02 +0100

    Modify the App class

--- app/src/main/java/ch/epfl/sweng/project/App.java
+++ app/src/main/java/ch/epfl/sweng/project/App.java
@@ -14,12 +14,15 @@ public class App extends Application {
         sInstance = this;
         graph = Graph.Initializer.init(false);
     }
+
     public static App getInstance() {
         return sInstance;
     }
+
     public Graph graph() {
         return graph;
     }
+
     public void setMockMode(boolean useMock) {
         graph = Graph.Initializer.init(useMock);
     }
