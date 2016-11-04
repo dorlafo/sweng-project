b46e3976591a299b3fb82f42cb680f2304704155
Amaury Combes <amaurycombes@hotmail.fr>
2016-11-04 23:39:48 +0100

    Add comments to the Graph class

--- app/src/debug/java/ch/epfl/sweng/project/Graph.java
+++ app/src/debug/java/ch/epfl/sweng/project/Graph.java
@@ -5,18 +5,41 @@ import javax.inject.Singleton;
 import ch.epfl.sweng.project.data.DebugDataModule;
 import dagger.Component;
 
+/**
+ * The Graph interface represents a mapping from the returns of the providers to the field of the
+ * injected activity
+ */
 @Singleton
 @Component(modules = {DebugDataModule.class})
 public interface Graph {
 
+    /**
+     * Injects providers return values to the BaseActivity
+     * @param activity the activity which will be injected
+     */
     void inject(BaseActivity activity);
 
+    /**
+     * Injects providers return values to the BaseListActivity
+     * @param activity the activity which will be injected
+     */
     void inject(BaseListActivity activity);
 
+    /**
+     * Injects providers return values to the BaseFragmentActivity
+     * @param activity the activity which will be injected
+     */
     void inject(BaseFragmentActivity activity);
 
-    void inject(InjectedBaseActivityTest test);
+    /**
+     * Injects providers return values to the InjectedBaseActivityTest
+     * @param testActivity the test activity which will be injected
+     */
+    void inject(InjectedBaseActivityTest testActivity);
 
+    /**
+     * Initializer of the graph
+     */
     public final static class Initializer {
         public static Graph init(boolean mockMode) {
             return DaggerGraph.builder().debugDataModule(new DebugDataModule(mockMode)).build();
