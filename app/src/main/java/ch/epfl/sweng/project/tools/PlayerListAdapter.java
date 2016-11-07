375b272c5073aaae3979ab62b91166801e4f7afc
leonardfavre <leonardmichel.favre@epfl.ch>
2016-11-07 19:30:19 +0100

    player list adapter for listing players

--- app/src/main/java/ch/epfl/sweng/project/tools/PlayerListAdapter.java
+++ app/src/main/java/ch/epfl/sweng/project/tools/PlayerListAdapter.java
@@ -1,8 +1,40 @@
 package ch.epfl.sweng.project.tools;
 
+import android.app.Activity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.firebase.database.FirebaseDatabase;
+
+import ch.epfl.sweng.project.R;
+import ch.epfl.sweng.project.model.Player;
+
 /**
  * Created by leo on 07.11.16.
  */
 
-public class PlayerListAdapter {
+public class PlayerListAdapter extends FirebaseListAdapter<Player> {
+
+    private final Activity activity;
+
+    public PlayerListAdapter(Activity activity) {
+        super(activity, Player.class, R.layout.list_element_player,
+                FirebaseDatabase.getInstance().getReference("players"));
+        this.activity = activity;
+    }
+
+
+
+    @Override
+    protected void populateView(View v, Player player, int position) {
+            TextView name = (TextView) v.findViewById(R.id.name);
+            TextView rank = (TextView) v.findViewById(R.id.rank_data);
+            TextView playerID = (TextView) v.findViewById(R.id.player_id_data);
+
+            name.setText(player.getFirstName() + " " + player.getLastName());
+            rank.setText(player.getRank().toString());
+            playerID.setText(player.getID().toString());
+
+    }
 }
