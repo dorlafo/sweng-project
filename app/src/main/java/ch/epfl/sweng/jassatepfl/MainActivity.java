c6cf41b0aca83d8d762a3f6836e7bcc411663738
panv <panv@hotmail.fr>
2016-11-10 22:36:34 +0100

    Made main activity extend navigation drawer
    Navigation drawer is now abstract. This is a temporary change, the
    structure of the activities will be changed when the refactoring of the
    activities is finished. Then navigation drawer will be incorporated to
    the base activity which all our activities extend.

--- app/src/main/java/ch/epfl/sweng/jassatepfl/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/jassatepfl/MainActivity.java
@@ -1,66 +1,32 @@
 package ch.epfl.sweng.jassatepfl;
 
+import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.webkit.CookieManager;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.iid.FirebaseInstanceId;
 
-public final class MainActivity extends AppCompatActivity {
+public final class MainActivity extends NavigationDrawerActivity {
 
     private FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View contentView = inflater.inflate(R.layout.activity_main, null, false);
+        drawer.addView(contentView, 0);
+
+        //setContentView(R.layout.activity_main);
 
         fAuth = FirebaseAuth.getInstance();
         System.out.println(FirebaseInstanceId.getInstance().getToken());
         //Show login screen if not logged in
         showLogin();
-    }
-
-    @Override
-    public void onBackPressed() {
-        //DO NOTHING -> it will disable the back button
-    }
-
-    // Creates Menu on top left corner
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.login_menu, menu);
-        return true;
-    }
-
-    /* Handler for menu items
-     * Usage of deprecated method because the new one require
-     * a minimum of android 21, we set minimum android 15
-     */
-    @Override
-    @SuppressWarnings("deprecation")
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.menu_logout:
-                if (Build.VERSION.SDK_INT < 21) {
-                    CookieManager.getInstance().removeAllCookie();
-                } else {
-                    CookieManager.getInstance().removeAllCookies(null);
-                }
-                //Sign out from Firebase
-                FirebaseAuth.getInstance().signOut();
-                //Show login activity
-                showLogin();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -70,7 +36,7 @@ public final class MainActivity extends AppCompatActivity {
 
         // Notification onClick handler.
         // Can not display match name because it doesn't exists anymore.
-        if(startIntent.hasExtra("notif") && startIntent.getStringExtra("notif").equals("matchexpired")) {
+        if (startIntent.hasExtra("notif") && startIntent.getStringExtra("notif").equals("matchexpired")) {
             new AlertDialog.Builder(this)
                     .setTitle(R.string.match_expired)
                     .show();
