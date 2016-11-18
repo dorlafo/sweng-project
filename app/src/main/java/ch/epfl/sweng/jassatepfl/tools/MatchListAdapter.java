bc6a4503e7c43024291a5dce5d3b57971a06020d
Alexis Montavon <alexis.montavon@epfl.ch>
2016-11-18 12:24:04 +0100

    cleaned MatchListAdapter

--- app/src/main/java/ch/epfl/sweng/jassatepfl/tools/MatchListAdapter.java
+++ app/src/main/java/ch/epfl/sweng/jassatepfl/tools/MatchListAdapter.java
@@ -1,14 +1,11 @@
 package ch.epfl.sweng.jassatepfl.tools;
 
-import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.NonNull;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import java.util.List;
