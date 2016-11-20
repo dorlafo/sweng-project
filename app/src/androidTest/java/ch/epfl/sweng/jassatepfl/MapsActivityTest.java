62ee7b38b52253503d7e3ce9242fceb2e73909fd
Amaury Combes <amaurycombes@hotmail.fr>
2016-11-20 17:50:42 +0100

    Uncomment some tests

--- app/src/androidTest/java/ch/epfl/sweng/jassatepfl/MapsActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/jassatepfl/MapsActivityTest.java
@@ -1,6 +1,30 @@
 package ch.epfl.sweng.jassatepfl;
 
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiSelector;
+
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import ch.epfl.sweng.jassatepfl.injections.InjectedBaseActivityTest;
+import ch.epfl.sweng.jassatepfl.model.Match;
+import ch.epfl.sweng.jassatepfl.model.Player;
+import ch.epfl.sweng.jassatepfl.test_utils.DummyData;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.contrib.DrawerMatchers.isClosed;
+import static android.support.test.espresso.contrib.DrawerMatchers.isOpen;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static ch.epfl.sweng.jassatepfl.test_utils.DBTestUtils.assertMatchContainsNPlayers;
+import static ch.epfl.sweng.jassatepfl.test_utils.DBTestUtils.assertMatchContainsPlayer;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 
 public final class MapsActivityTest extends InjectedBaseActivityTest {
 
@@ -11,7 +35,6 @@ public final class MapsActivityTest extends InjectedBaseActivityTest {
     @Override
     public void setUp() throws Exception {
         super.setUp();
-        getActivity();
     }
 
     /* Not working for now
@@ -21,19 +44,26 @@ public final class MapsActivityTest extends InjectedBaseActivityTest {
         matches.add(DummyData.twoPlayersMatch());
         matches.add(DummyData.onePlayerMatch());
         dbRefWrapMock.addMatches(matches);
+
+        getActivity();
+
         onView(withId(R.id.drawer_layout)).check(matches(isClosed()));
         onView(withId(R.id.maps_menu_button)).perform(click());
         onView(withId(R.id.drawer_layout)).check(matches(isOpen()));
         dbRefWrapMock.reset();
-    }
+    }*/
 
 
+    /*
     @Test
     public void testMarkerDisplaysDialog() {
         Set<Match> matches = new HashSet<>();
         matches.add(DummyData.twoPlayersMatch());
         matches.add(DummyData.onePlayerMatch());
         dbRefWrapMock.addMatches(matches);
+
+        getActivity();
+
         UiDevice device = UiDevice.getInstance(getInstrumentation());
         UiObject marker = device.findObject(new UiSelector().descriptionContains("Rolex"));
         try {
@@ -47,13 +77,16 @@ public final class MapsActivityTest extends InjectedBaseActivityTest {
             fail();
         }
         dbRefWrapMock.reset();
-    }
+    }*/
 
-    @Test
+    /*@Test
     public void testAddPlayerOnMatchActivity() {
         Set<Match> matches = new HashSet<>();
         matches.add(DummyData.onePlayerMatch());
         dbRefWrapMock.addMatches(matches);
+
+        getActivity();
+
         UiDevice device = UiDevice.getInstance(getInstrumentation());
         UiObject marker = device.findObject(new UiSelector().descriptionContains("Rolex"));
         assertMatchContainsNPlayers(dbRefWrapMock, "one_player", 1);
@@ -75,6 +108,9 @@ public final class MapsActivityTest extends InjectedBaseActivityTest {
         Set<Match> matches = new HashSet<>();
         matches.add(DummyData.fullMatch());
         dbRefWrapMock.addMatches(matches);
+
+        getActivity();
+
         assertMatchContainsNPlayers(dbRefWrapMock, "full", 4);
         UiDevice device = UiDevice.getInstance(getInstrumentation());
         UiObject marker = device.findObject(new UiSelector().descriptionContains("CO"));
@@ -96,6 +132,9 @@ public final class MapsActivityTest extends InjectedBaseActivityTest {
         Set<Match> matches = new HashSet<>();
         matches.add(DummyData.onePlayerMatch());
         dbRefWrapMock.addMatches(matches);
+
+        getActivity();
+
         UiDevice device = UiDevice.getInstance(getInstrumentation());
         UiObject marker = device.findObject(new UiSelector().descriptionContains("Rolex"));
         assertMatchContainsNPlayers(dbRefWrapMock, "one_player", 1);
