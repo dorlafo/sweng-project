4ed5cadd6b25615a044807903131d04164053c7b
dorlafo <dorian.laforest@epfl.ch>
2016-11-22 11:25:55 +0100

    Modify PlayerListAdapter to show the team the player is in if the adapter has a match passed as parameter

--- app/src/main/java/ch/epfl/sweng/jassatepfl/tools/PlayerListAdapter.java
+++ app/src/main/java/ch/epfl/sweng/jassatepfl/tools/PlayerListAdapter.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 import java.util.List;
 
 import ch.epfl.sweng.jassatepfl.R;
+import ch.epfl.sweng.jassatepfl.model.Match;
 import ch.epfl.sweng.jassatepfl.model.Player;
 
 /**
@@ -20,27 +21,49 @@ import ch.epfl.sweng.jassatepfl.model.Player;
  */
 public class PlayerListAdapter extends ArrayAdapter<Player> {
 
-    public PlayerListAdapter(Context context, int resource, List<Player> items) {
-        super(context, resource, items);
+    private List<Player> players;
+    private Match match;
+
+    public PlayerListAdapter(Context context, int resource, List<Player> players, Match match) {
+        super(context, resource, players);
+        this.players = players;
+        this.match = match;
+    }
+
+    public PlayerListAdapter(Context context, int resource, List<Player> players) {
+        this(context, resource, players, null);
+    }
+
+    @Override
+    public int getCount() {
+        return players.size();
+    }
+
+    @Override
+    public Player getItem(int position) {
+        return players.get(position);
     }
 
     @Override
     @NonNull
     public View getView(int position, View convertView, @NonNull ViewGroup parent) {
-        View v = convertView;
-        if (v == null) {
-            LayoutInflater inflater = LayoutInflater.from(getContext());
-            v = inflater.inflate(R.layout.player_list_element, null);
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.player_list_element, null);
         }
 
         Player p = getItem(position);
-        if (p != null) {
-            TextView tv = (TextView) v.findViewById(R.id.player_name);
-            if (tv != null) {
-                tv.setText(p.toString());
-            }
+
+        TextView playerName = (TextView) convertView.findViewById(R.id.player_name);
+        if(match == null) {
+            playerName.setText(p.toString());
         }
-        return v;
+        else {
+            playerName.setText(p.toString() + " : team " + match.teamNbForPlayer(p));
+        }
+
+
+        return convertView;
     }
 
 }
