f7564a6e063d35d27ce77e45ebcde425d0ace561
Amaury Combes <amaurycombes@hotmail.fr>
2016-11-22 16:44:03 +0100

    Move mocks injections to the androidTest folder

---
+++ app/src/androidTest/java/ch/epfl/sweng/jassatepfl/test_utils/injections/FakeGraph.java
@@ -0,0 +1,20 @@
+package ch.epfl.sweng.jassatepfl.test_utils.injections;
+
+import javax.inject.Singleton;
+
+import ch.epfl.sweng.jassatepfl.InjectedBaseActivityTest;
+import ch.epfl.sweng.jassatepfl.injections.Graph;
+import dagger.Component;
+
+/**
+ * @author Amaury Combes
+ */
+@Singleton
+@Component(modules = {
+        FakeModules.class
+})
+public interface FakeGraph extends Graph {
+
+    void inject(InjectedBaseActivityTest activityTest);
+
+}
---
+++ app/src/androidTest/java/ch/epfl/sweng/jassatepfl/test_utils/injections/FakeModules.java
@@ -0,0 +1,66 @@
+package ch.epfl.sweng.jassatepfl.test_utils.injections;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
+import javax.inject.Singleton;
+
+import ch.epfl.sweng.jassatepfl.database.helpers.DBReferenceWrapper;
+import ch.epfl.sweng.jassatepfl.test_utils.database.local.Root;
+import ch.epfl.sweng.jassatepfl.test_utils.mocks.DBRefWrapMock;
+import ch.epfl.sweng.jassatepfl.model.Player;
+import ch.epfl.sweng.jassatepfl.model.Rank;
+import dagger.Module;
+import dagger.Provides;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+/**
+ * @author Amaury Combes
+ */
+@Module
+public class FakeModules {
+
+    /**
+     * A DatabaseReference provider
+     *
+     * @return returns a DatabaseReference that can be mocked or not
+     */
+    @Provides
+    @Singleton
+    public DBReferenceWrapper provideDBReference() {
+            DBRefWrapMock dbRefWrapMock = new DBRefWrapMock(FirebaseDatabase.getInstance().getReference());
+            fillDB(dbRefWrapMock);
+            return dbRefWrapMock;
+    }
+
+    /**
+     * A firebase authentification provider
+     *
+     * @return returns a firebase authentification that can be mocked or not
+     */
+    @Provides
+    @Singleton
+    public FirebaseAuth provideDBAuth() {
+            FirebaseAuth fAuth = mock(FirebaseAuth.class);
+            addMockedBehaviorAuth(fAuth);
+            return fAuth;
+    }
+
+    private void fillDB(DBRefWrapMock dbRef) {
+        Root root = (Root) dbRef.getCurrentNode();
+        root.initialize();
+        root.getChild("players")
+                .addChild("696969")
+                .setData(new Player(new Player.PlayerID("696969"), "LeBricoleur", "Bob", new Rank(1000)));
+    }
+
+    private void addMockedBehaviorAuth(FirebaseAuth fAuth) {
+        FirebaseUser fUser = mock(FirebaseUser.class);
+        when(fUser.getDisplayName()).thenReturn("696969");
+        when(fAuth.getCurrentUser()).thenReturn(fUser);
+    }
+}
