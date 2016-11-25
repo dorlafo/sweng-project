047daa61e78a510d2f9577ace5fabf654b5e77c4
dorlafo <dorian.laforest@epfl.ch>
2016-11-25 01:07:09 +0100

    Refactor PlayerListAdapter and make it using a refresh method instead of being recreated every time

--- app/src/main/java/ch/epfl/sweng/jassatepfl/tools/PlayerListAdapter.java
+++ app/src/main/java/ch/epfl/sweng/jassatepfl/tools/PlayerListAdapter.java
@@ -5,9 +5,11 @@ import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsoluteLayout;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ch.epfl.sweng.jassatepfl.R;
@@ -23,15 +25,17 @@ public class PlayerListAdapter extends ArrayAdapter<Player> {
 
     private List<Player> players;
     private Match match;
+    private List<String> playersReady;
 
-    public PlayerListAdapter(Context context, int resource, List<Player> players, Match match) {
+    public PlayerListAdapter(Context context, int resource, List<Player> players, Match match, List<String> playersReady) {
         super(context, resource, players);
         this.players = players;
         this.match = match;
+        this.playersReady = playersReady;
     }
 
     public PlayerListAdapter(Context context, int resource, List<Player> players) {
-        this(context, resource, players, null);
+        this(context, resource, players, null, new ArrayList<String>());
     }
 
     @Override
@@ -53,20 +57,47 @@ public class PlayerListAdapter extends ArrayAdapter<Player> {
         }
 
         Player p = getItem(position);
-
         TextView playerName = (TextView) convertView.findViewById(R.id.player_name);
         if(match == null) {
             playerName.setText(p.toString());
         }
-        else if (match.teamNbForPlayer(p) == -1){
-            playerName.setText(p.toString() + " : no team assigned yet");
-        }
         else {
-            playerName.setText(p.toString() + " : team " + match.teamNbForPlayer(p));
+            if (match.teamNbForPlayer(p) == -1){
+                playerName.setText(getFirstFirstName(p.getFirstName()) + " : no team assigned yet");
+            }
+            else {
+                playerName.setText(getFirstFirstName(p.getFirstName()) + " : team " + match.teamNbForPlayer(p));
+            }
+
+            if(playersReady.contains(Integer.toString(position))) {
+                playerName.setBackgroundColor(0xFF00FF00);
+            }
+            else {
+                playerName.setBackgroundColor(0xFFFFFFFF);
+            }
         }
 
 
         return convertView;
+    }
+
+    private String getFirstFirstName(String name) {
+        int indexSpace = name.indexOf(' ');
+        if(indexSpace == -1) {
+            return name;
+        }
+        else {
+            return name.substring(0, indexSpace);
+        }
+    }
+
+    public void refreshData(List<Player> p, Match m, List<String> pr) {
+        this.players.clear();
+        this.players.addAll(p);
+        this.match = m;
+        this.playersReady.clear();
+        this.playersReady.addAll(pr);
+        notifyDataSetChanged();
     }
 
 }
