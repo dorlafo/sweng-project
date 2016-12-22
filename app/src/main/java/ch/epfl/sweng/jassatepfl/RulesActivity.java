8e7fedeff94bbaee9ebbf7ff636137d59a701678
panv <panv@hotmail.fr>
2016-12-22 03:05:08 +0100

    Move variant spinner to toolbar in rules activity
    Bugs: text is cut, tests probably won't pass

--- app/src/main/java/ch/epfl/sweng/jassatepfl/RulesActivity.java
+++ app/src/main/java/ch/epfl/sweng/jassatepfl/RulesActivity.java
@@ -1,30 +1,37 @@
 package ch.epfl.sweng.jassatepfl;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.mukesh.MarkdownView;
 
 import ch.epfl.sweng.jassatepfl.model.Match;
 
+import static android.support.v7.widget.ListPopupWindow.WRAP_CONTENT;
+import static android.util.TypedValue.COMPLEX_UNIT_SP;
+
 /**
- * @author Alexis Montavon
- *
  * Activity class allowing user to read the rules of the game
  * for all variants.
+ *
+ * @author Alexis Montavon
  */
 public class RulesActivity extends BaseActivityWithNavDrawer {
+
     private MarkdownView userGuideView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_rules);
         LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View contentView = inflater.inflate(R.layout.activity_rules, drawer, false);
         drawer.addView(contentView, 0);
@@ -34,43 +41,49 @@ public class RulesActivity extends BaseActivityWithNavDrawer {
                 android.R.layout.simple_spinner_item, Match.GameVariant.values());
         variantAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        Spinner variantSpinner = (Spinner) findViewById(R.id.rules_spinner);
+        Spinner variantSpinner = new Spinner(this);
         variantSpinner.setAdapter(variantAdapter);
         variantSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                TextView textView = ((TextView) parent.getChildAt(0));
+                textView.setTextColor(Color.WHITE);
+                textView.setTextSize(COMPLEX_UNIT_SP, 20f);
                 Object item = parent.getItemAtPosition(position);
-                if(parent.getId() == R.id.rules_spinner) {
-                    switch((Match.GameVariant)item) {
-                        case CHIBRE:
-                            userGuideView.loadMarkdownFromAssets("ChibreRules.md");
-                            break;
-                        case PIQUE_DOUBLE:
-                            userGuideView.loadMarkdownFromAssets("PiqueDoubleRules.md");
-                            break;
-                        case OBEN_ABE:
-                            userGuideView.loadMarkdownFromAssets("ObenAbeRules.md");
-                            break;
-                        case UNDEN_UFE:
-                            userGuideView.loadMarkdownFromAssets("UndenUfeRules.md");
-                            break;
-                        case SLALOM:
-                            userGuideView.loadMarkdownFromAssets("SlalomRules.md");
-                            break;
-                        case CHICANE:
-                            userGuideView.loadMarkdownFromAssets("ChicaneRules.md");
-                            break;
-                        case JASS_MARANT:
-                            userGuideView.loadMarkdownFromAssets("JassMarantRules.md");
-                            break;
-                    }
+                switch ((Match.GameVariant) item) {
+                    case CHIBRE:
+                        userGuideView.loadMarkdownFromAssets("ChibreRules.md");
+                        break;
+                    case PIQUE_DOUBLE:
+                        userGuideView.loadMarkdownFromAssets("PiqueDoubleRules.md");
+                        break;
+                    case OBEN_ABE:
+                        userGuideView.loadMarkdownFromAssets("ObenAbeRules.md");
+                        break;
+                    case UNDEN_UFE:
+                        userGuideView.loadMarkdownFromAssets("UndenUfeRules.md");
+                        break;
+                    case SLALOM:
+                        userGuideView.loadMarkdownFromAssets("SlalomRules.md");
+                        break;
+                    case CHICANE:
+                        userGuideView.loadMarkdownFromAssets("ChicaneRules.md");
+                        break;
+                    case JASS_MARANT:
+                        userGuideView.loadMarkdownFromAssets("JassMarantRules.md");
+                        break;
                 }
             }
+
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
                 userGuideView.loadMarkdownFromAssets("ChibreRules.md");
             }
         });
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.addView(variantSpinner, new Toolbar.LayoutParams(WRAP_CONTENT, WRAP_CONTENT, Gravity.END));
     }
+
 }
